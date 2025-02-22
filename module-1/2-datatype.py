0d88b152be24c371bf245a70c4f0f99ccb20c0d1
adeayo1144 <adeayo1144@gmail.com>
2025-02-22 14:07:02 +0100

    Tutorial

---
+++ module-1/2-datatype.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" a file that contains datatypes"""
+num = 3
+street = "okin block industry"
+answer = False
+amount = 500.00
+sci = 1e-2
+print(type(num))
+print(type(street))
+print(type(answer))
+print(type(amount))
+print(type(sci))
---
+++ module-1/3-input.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""a file that handles user input"""
+name = input("enter your fullname: ")
+print(name)
