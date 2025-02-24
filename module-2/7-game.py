38bdec9396b55d8648b5a11efbea735cd6e113cf
adeayo1144 <adeayo1144@gmail.com>
2025-02-24 19:46:01 +0100

    class

--- module-2/7-game.py
+++ module-2/7-game.py
@@ -1,4 +1,7 @@
+#/usr/bin/python3
 import random
+
+
 
 def get_choice():
     Player_choice = input("Enter a choice (rock,paper, scissors):")
@@ -6,6 +9,7 @@ def get_choice():
     computer_choice = random.choice(options)
     choices = {"player": Player_choice, "computer": computer_choice}
     return choices
+
 def check_win (player, computer):
     print(f"you choose {player}, computer choose{computer}")
     if player == ("computer"):
@@ -18,16 +22,17 @@ def check_win (player, computer):
     if player == "paper":
         if computer =="rock":
             return "paper covers rock!you win!"
-    else:
-        return "scissors cuts paper!you lose."
-        if player == "scissors":
-            if computer == "paper":
-                return "scissors cuts paper! you win!"
-            else:
-                return "Rock smashes scissors! you lose."
-        choices = get_choices()
-        result = check_win(choices ["player"], choices["computer"])
-        print(result)
+        else:
+            return "scissors cuts paper!you lose."
+    elif player == "scissors":
+        if computer == "paper":
+            return "scissors cuts paper! you win!"
+        else:
+            return "Rock smashes scissors! you lose."
+        
+choices = get_choice()
+result = check_win(choices ["player"], choices["computer"])
+print(result)
 
 
 
