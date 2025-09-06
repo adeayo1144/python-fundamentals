135dff0ddc27dcf967afe160ad9ffa7568ed093b
etim-ipee <imaikopelijah001@gmail.com>
2025-09-06 11:31:43 +0100

    commit

---
+++ module-D/8- create linkedlist.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""a module that handle the creation of a linkedlist"""
+
+class Node:
+    """This clss is the node in a linked list"""
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class linkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end = " ->")
+            current = current.next
+        print("None")
+
+if __name__ == "__main__":
+    my_list = linkedList()
+    my_list.append(1)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.print_list()
